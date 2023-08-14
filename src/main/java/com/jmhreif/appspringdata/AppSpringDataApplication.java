eedd966f09ffdcdebd6dbb3f025d61a7b8b9586f
Jennifer Reif <jennifer.reif@neo4j.com>
2023-08-14 16:56:36 -0500

    Initial commit

---
+++ src/main/java/com/jmhreif/appspringdata/AppSpringDataApplication.java
@@ -0,0 +1,13 @@
+package com.jmhreif.appspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmhreif/appspringdata/AppSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmhreif.appspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
