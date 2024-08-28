09648af7ec89946d0ae98e2942f5acad43156236
Jennifer Reif <jennifer.reif@neo4j.com>
2024-08-28 14:35:57 -0500

    Add verify connectivity test

--- src/test/java/com/example/appspringdata/AppSpringDataApplicationTests.java
+++ src/test/java/com/example/appspringdata/AppSpringDataApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.appspringdata;
 
 import org.junit.jupiter.api.Test;
+import org.neo4j.driver.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AppSpringDataApplicationTests {
-
-	@Test
-	void contextLoads() {
+	// tag::verify[]
+	final Driver driver;
+	public AppSpringDataApplicationTests(@Autowired Driver driver) {
+		this.driver = driver;
 	}
 
+	@Test
+	final void testConnection() {
+		driver.verifyConnectivity();
+	}
+	// end::verify[]
 }
